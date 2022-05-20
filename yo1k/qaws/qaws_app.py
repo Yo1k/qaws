48c1152ac971cb5ce55c92907678b8f51d14faf4
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2022-05-20 19:19:52 +0300

    Optimize the insertion of unique questions to the DB

--- yo1k/qaws/qaws_app.py
+++ yo1k/qaws/qaws_app.py
@@ -6,6 +6,7 @@ from flask import Flask, abort, current_app
 from flask import request, g
 import psycopg2  # type: ignore
 import psycopg2.errors  # type: ignore
+import psycopg2.extras # type: ignore
 
 app: Flask = Flask(__name__)
 
@@ -19,10 +20,11 @@ def get_questions(num: int) -> list[dict[str, Any]]:
 def get_db():  # type: ignore
     if "db" not in g:
         g.db = psycopg2.connect(  # pylint: disable=E0237
-                dbname=os.environ.get("POSTGRES_NAME"),
-                user=os.environ.get("POSTGRES_USER"),
-                password=os.environ.get("POSTGRES_PASSWORD"),
-                host="db",
+                dbname="postgres",
+                user="postgres",
+                password="postgres",
+                # host="db",
+                host="127.0.0.1",
                 port="5432")
         with g.db:
             with g.db.cursor() as cur:
@@ -30,31 +32,35 @@ def get_db():  # type: ignore
                     cur.execute(schema.read())
     return g.db
 
-
+# SKTODO make count for returning
 def prepare_updqstn() -> None:
     with g.db:
         with g.db.cursor() as cur:
             cur.execute(
                     """
-                    PREPARE updqstn (integer, text, text, timestamptz) AS
-                        INSERT INTO questions VALUES($1, $2, $3, $4);
+                    PREPARE updqstn (int[], text[], text[], timestamptz[]) AS
+                        INSERT INTO questions
+                        (SELECT * from unnest($1, $2, $3, $4))
+                        ON CONFLICT (id) DO NOTHING
+                        RETURNING count(id);
                     """)
 
 
-def insert_question(id_q: int, question: str, answer: str, created_at: str) -> bool:
-    try:
-        with g.db:
-            with g.db.cursor() as cur:
-                cur.execute(
-                        "EXECUTE updqstn(%(id)s, %(question)s, %(answer)s, %(created_at)s);",
-                        {
-                                "id": id_q,
-                                "question": question,
-                                "answer": answer,
-                                "created_at": created_at})
-                return False
-    except psycopg2.errors.lookup("23505"):
-        return True
+def reformat_information(raw_data):
+    key_list = ["id", "question", "answer", "created_at"]
+    return [[row[key] for row in raw_data] for key in key_list]
+
+
+def insert_questions(data) -> int:
+    # with g.db:
+    with g.db.cursor() as cur:
+        cur.execute(
+                "EXECUTE updqstn (%s, %s, %s, %s::timestamptz[]);",
+                data)
+        returning: list[tuple[Any]] = cur.fetchall()
+    print(returning)
+    print(len(returning))
+    return len(returning)
 
 
 @app.teardown_request
@@ -62,11 +68,12 @@ def close_db(_=None) -> None:  # type: ignore
     db = g.pop("db", None)
 
     if db is not None:
+        db.commit()
         db.close()
 
 
 @app.route('/', methods=['POST'])
-def questions_request() -> Union[str, dict[None, None], None]:
+def request_questions() -> Union[str, dict[None, None], None]:
     if request.method == "POST":
         questions_num: int = request.get_json().get('questions_num')  # type: ignore
         assert isinstance(questions_num, int) and questions_num >= 0, \
@@ -75,25 +82,20 @@ def questions_request() -> Union[str, dict[None, None], None]:
             return {}
         else:
             questions_list = get_questions(questions_num)
+            prep_data = reformat_information(questions_list)
+
             get_db()  # type: ignore
             prepare_updqstn()
 
-            retries: int = 1000
+            retries: int = 100
             while retries > 0:
                 retries -= 1
-                fail_uniq_num: int = 0
-
-                for qstn in questions_list:
-                    fail_uniq = insert_question(
-                            qstn['id'],
-                            qstn['question'],
-                            qstn['answer'],
-                            qstn['created_at'])
-                    if fail_uniq:
-                        fail_uniq_num += 1
+                fail_uniq_num = questions_num - insert_questions(prep_data)
+                questions_num = fail_uniq_num
 
                 if fail_uniq_num:
                     questions_list = get_questions(fail_uniq_num)
+                    prep_data = reformat_information(questions_list)
                 else:
                     return f"{questions_list[-1]['question']}"
 
