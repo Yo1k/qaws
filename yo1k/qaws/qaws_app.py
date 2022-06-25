184030a4edbd764a17be0790e7ad0ee41d572884
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2022-06-25 15:04:41 +0300

    OOP representation (not final version)

--- yo1k/qaws/qaws_app.py
+++ yo1k/qaws/qaws_app.py
@@ -1,80 +1,78 @@
 import urllib.request
 import json
-from abc import ABC
-from typing import Any, Union, Optional, Generic, TypeVar, Sequence
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Any, Optional, Generic, TypeVar, Sequence, NamedTuple, MutableSequence, Union
 from flask import Flask, abort, current_app
-from flask import request, g
-import psycopg
-import psycopg.errors
+from flask import request
+from psycopg import Connection
+from psycopg_pool import ConnectionPool
 
 
-T = TypeVar("T")
+T_co = TypeVar("T_co", covariant=True)
 JSONType = dict[str, Any]
 
 
-class QuestionService(ABC, Generic[T]):
-    def get_questions(self, num: int) -> Sequence[T]:
+class PreparedQuestions(NamedTuple):
+    id: MutableSequence[int]
+    question: MutableSequence[str]
+    answer: MutableSequence[str]
+    created_at: MutableSequence[datetime]
+
+
+class QuestionService(ABC, Generic[T_co]):
+    @abstractmethod
+    def get_questions(self, num: int) -> T_co:
         pass
 
 
 class StorageService(ABC):
-    def insert_uniq_questions(self, data: list[list[Any]]):
+    @abstractmethod
+    def insert_uniq_questions(self, questions: PreparedQuestions) -> int:
         pass
 
 
-class JSONQuestionService(QuestionService[JSONType]):
+class JSONQuestionService(QuestionService[Sequence[JSONType]]):
     def __init__(self, url="https://jservice.io/api/random?count="):
-        self.__url = url
+        self.__url: str = url
 
     def get_questions(self, num: int) -> Sequence[JSONType]:
         with urllib.request.urlopen(url=f"{self.__url}{num}") as response:
-            data: Sequence[JSONType] = json.loads(response.read())
-        return data
+            return json.loads(response.read())
 
 
-class DefaultQuestionService(QuestionService[Sequence[Any]]): #SKTODO ABC
-    def __init__(self, delegate: JSONQuestionService):
-        self.__delegate = delegate
-        self.__cache_json_data = []  # SKTODO choose the right data structure
+class DefaultQuestionService(QuestionService[PreparedQuestions]):
+    def __init__(self, delegate: QuestionService[Sequence[JSONType]]):
+        self.__delegate: QuestionService[Sequence[JSONType]] = delegate
 
-    def get_last_question(self) -> Union[str, list]:
-        if self.__cache_json_data:
-            return self.__cache_json_data[-1]["question"]
-        else:
-            assert False, f"{self.__cache_json_data}"
+    def get_questions(self, num: int) -> PreparedQuestions:
+        return DefaultQuestionService.__prepare_questions(
+                self.__delegate.get_questions(num))
 
-    def get_questions(self, num: int) -> Sequence[Sequence[Any]]:
-        self.__fetch_raw_data(num)
-        return self.__reformat_information()
-
-    def __fetch_raw_data(self, num: int) -> None:
-        self.__cache_json_data = self.__delegate.get_questions(num)
-
-    # SKTODO rebuild to nametuple[MutSequese[Any]]
-    def __reformat_information(self) -> Sequence[Sequence[Any]]:
-        key_list: Sequence[str] = ["id", "question", "answer", "created_at"]
-        return [[row[key] for row in self.__cache_json_data] for key in key_list]
+    @staticmethod
+    def __prepare_questions(questions: Sequence[JSONType]) -> PreparedQuestions:
+        prep_questions = PreparedQuestions([], [], [], [])
+        for question in questions:
+            prep_questions.id.append(question["id"])
+            prep_questions.question.append(question["question"])
+            prep_questions.answer.append(question["answer"])
+            prep_questions.created_at.append(question["created_at"])
+        return prep_questions
 
 
 class PostgresqlStorageService(StorageService):
-    def __init__(self, schema="./yo1k/qaws/questions_schema.sql"):
-        self.schema = schema
-        self.__conn = psycopg.connect(
-                dbname="postgres",
-                user="postgres",
-                password="postgres",
-                # host="db",
-                host="127.0.0.1",
-                port="5432")
-        self.__create_table()
+    def __init__(self, conn_pool: ConnectionPool):
+        self.__conn_pool: ConnectionPool = conn_pool
+        self.__conn: Connection = self.__conn_pool.getconn()
+        self.__init_schema()
 
-    def __create_table(self):
+    def __init_schema(self) -> None:
         with self.__conn.transaction():
-            with self.__conn.cursor() as cur:
-                with open(self.schema) as schema:
+            with self.__conn.cursor() as cur:  # SKTODO remove cursor
+                with current_app.open_resource('schema.sql') as schema:
                     cur.execute(schema.read())
 
-    def insert_uniq_questions(self, data: list[list[Any]]) -> int:
+    def insert_uniq_questions(self, questions: PreparedQuestions) -> int:
         with self.__conn.cursor() as cur:
             cur.execute(
                     """
@@ -88,50 +86,81 @@ class PostgresqlStorageService(StorageService):
                         ON CONFLICT (id) DO NOTHING
                         RETURNING id)
                     SELECT COUNT(*) FROM inserted;""",
-                    data)
+                    questions)
             returning: list[tuple[Any]] = cur.fetchall()
         return returning[0][0]
 
-    def finalize(self) -> None:
-        self.__conn.commit()
-        self.__conn.close()
+    def close(self) -> None:
+        try:
+            self.__conn.commit()
+        finally:
+            self.__conn_pool.putconn(self.__conn)
 
 
-def init_flask_app_context():
-    # config = app.config
-    g.db_service = PostgresqlStorageService()
-    g.client = JSONQuestionService()
-    g.questions_service = DefaultQuestionService(g.client)
+class QAWS:
+    def __init__(self, conn_pool: ConnectionPool):
+        # SKTODO add type hints
+        self.__conn_pool: ConnectionPool = conn_pool
+        # self.db_service: Optional[PostgresqlStorageService] = None
+        # self.client: Optional[JSONQuestionService] = None
+        # self.questions_service: Optional[DefaultQuestionService] = None
+        self.db_service: Optional[PostgresqlStorageService] = PostgresqlStorageService(conn_pool)
+        self.client = JSONQuestionService()
+        self.questions_service = DefaultQuestionService(self.client)
 
+    # def before_request(self) -> None:
+    #     self.db_service = PostgresqlStorageService(self.__conn_pool)
+    #     self.client = JSONQuestionService()
+    #     self.questions_service = DefaultQuestionService(self.client)
 
-def create_app():
-    app = Flask(__name__)
-
-    app.before_request(init_flask_app_context)
-
-    @app.teardown_request
-    def final(e=None):
-        print(f"{'questions_service' in g}")
-        g.db_service.finalize()
-
-    @app.route('/', methods=['POST'])
-    def request_questions() -> Union[str, dict[None, None], None]:
-        # if request.method == "POST":
-        questions_num: int = request.get_json().get('questions_num')  # type: ignore
+    # SKTODO add type hints
+    def request_questions(self) -> Union[str, dict]:
+        questions_num: int = request.get_json().get('questions_num')
         assert isinstance(questions_num, int) and questions_num >= 0, \
             f"questions_num={questions_num}"
         if questions_num == 0:
             return {}
         else:
-            prep_data = g.questions_service.get_questions(questions_num)
+            questions = self.questions_service.get_questions(questions_num)
             retries: int = 100
             while retries > 0:
                 retries -= 1
-                fail_uniq_num = questions_num - g.db_service.insert_uniq_questions(prep_data)
+                fail_uniq_num = questions_num - self.db_service.insert_uniq_questions(questions)
                 if fail_uniq_num:
-                    prep_data = g.questions_service.get_questions(fail_uniq_num)
+                    questions = self.questions_service.get_questions(fail_uniq_num)
+                    questions_num = fail_uniq_num
                 else:
-                    return g.questions_service.get_last_question()
+                    return questions.question[-1]
             abort(500)
 
+    def close(self, e=None):  # SKTODO understand necessity of `e=None`
+        self.db_service.close()
+
+
+__pool = ConnectionPool(
+        conninfo="dbname='postgres'"
+                 "user='postgres'"
+                 "password='postgres'"
+                 "host='127.0.0.1'"
+                 "port='5432'")
+
+
+def create_app():
+    app = Flask(__name__)
+    qaws = QAWS(conn_pool=__pool)
+
+    # app.before_request(qaws.before_request)  # SKTODO remove
+    app.teardown_request(qaws.close)
+    app.add_url_rule(
+            rule="/",
+            methods=['POST'],
+            view_func=qaws.request_questions)
+
     return app
+
+# SKTODO
+# 1) change StorageService to using context manager for connection, transaction
+# 2) add dependency injection to QAWS: PostgresqlStorageService, DefaultQuestionServiceб JSONQuestionService
+# 3) add parameter `conn` to `request_questions`, `insert_uniq_questions`
+
+# add to `readme.md`: `export FLASK_APP="yo1k.qaws.qaws_app:create_app()"`
