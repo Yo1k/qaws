1f8d09c4e4ce9b5e65b7ff6b4aa942380bf368e2
Stanislav Kovalenko <stanislav.l.kovalenko@gmail.com>
2022-07-04 04:00:21 +0300

    Do OOP refactoring

--- yo1k/qaws/qaws_app.py
+++ yo1k/qaws/qaws_app.py
@@ -1,101 +1,155 @@
-import os
+import atexit
 import urllib.request
 import json
-from typing import Any, Union
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Any, Generic, TypeVar, Sequence, NamedTuple, MutableSequence, Union, Callable
 from flask import Flask, abort, current_app
-from flask import request, g
-import psycopg2  # type: ignore
-import psycopg2.errors  # type: ignore
-
-app: Flask = Flask(__name__)
+from flask import request
+from psycopg import Connection
+from psycopg_pool import ConnectionPool
 
 
-def get_questions(num: int) -> list[dict[str, Any]]:
-    with urllib.request.urlopen(url=f"https://jservice.io/api/random?count={num}") as response:
-        data: list[dict[str, Any]] = json.loads(response.read())
-    return data
+T_co = TypeVar("T_co", covariant=True)
+JSONType = dict[str, Any]
 
 
-def get_db():  # type: ignore
-    if "db" not in g:
-        g.db = psycopg2.connect(  # pylint: disable=E0237
-                dbname=os.environ.get("POSTGRES_NAME"),
-                user=os.environ.get("POSTGRES_USER"),
-                password=os.environ.get("POSTGRES_PASSWORD"),
-                host="db",
-                port="5432")
-        with g.db:
-            with g.db.cursor() as cur:
-                with current_app.open_resource("questions_schema.sql") as schema:
-                    cur.execute(schema.read())
-    return g.db
+class PreparedQuestions(NamedTuple):
+    id: MutableSequence[int]
+    question: MutableSequence[str]
+    answer: MutableSequence[str]
+    created_at: MutableSequence[datetime]
 
 
-def prepare_updqstn() -> None:
-    with g.db:
-        with g.db.cursor() as cur:
+class QuestionService(ABC, Generic[T_co]):
+    @abstractmethod
+    def get_questions(self, num: int) -> T_co:
+        pass
+
+
+class StorageService(ABC):
+    @abstractmethod
+    def insert_uniq_questions(self, questions: PreparedQuestions, conn: Connection) -> int:
+        pass
+
+
+class JSONQuestionService(QuestionService[Sequence[JSONType]]):
+    def __init__(self, url: str = "https://jservice.io/api/random?count=") -> None:
+        self.__url: str = url
+
+    def get_questions(self, num: int) -> Sequence[JSONType]:
+        with urllib.request.urlopen(url=f"{self.__url}{num}") as response:
+            return json.loads(response.read())
+
+
+class DefaultQuestionService(QuestionService[PreparedQuestions]):
+    def __init__(self, delegate: QuestionService[Sequence[JSONType]]):
+        self.__delegate: QuestionService[Sequence[JSONType]] = delegate
+
+    def get_questions(self, num: int) -> PreparedQuestions:
+        return DefaultQuestionService.__prepare_questions(
+                self.__delegate.get_questions(num))
+
+    @staticmethod
+    def __prepare_questions(questions: Sequence[JSONType]) -> PreparedQuestions:
+        prep_questions = PreparedQuestions([], [], [], [])
+        for question in questions:
+            prep_questions.id.append(question["id"])
+            prep_questions.question.append(question["question"])
+            prep_questions.answer.append(question["answer"])
+            prep_questions.created_at.append(question["created_at"])
+        return prep_questions
+
+
+class PgStorageService(StorageService):
+    def insert_uniq_questions(self, questions: PreparedQuestions, conn: Connection) -> int:
+        with conn.cursor() as cur:
             cur.execute(
                     """
-                    PREPARE updqstn (integer, text, text, timestamptz) AS
-                        INSERT INTO questions VALUES($1, $2, $3, $4);
-                    """)
+                    WITH inserted AS (
+                        INSERT INTO questions
+                        (SELECT * FROM unnest(
+                                %s::int[],
+                                %s::text[],
+                                %s::text[],
+                                %s::timestamptz[]))
+                        ON CONFLICT (id) DO NOTHING
+                        RETURNING id)
+                    SELECT COUNT(*) FROM inserted;""",
+                    questions)
+            returning: list[tuple[Any]] = cur.fetchall()
+            inserted_count: int = returning[0][0]
+            return inserted_count
 
 
-def insert_question(id_q: int, question: str, answer: str, created_at: str) -> bool:
-    try:
-        with g.db:
-            with g.db.cursor() as cur:
-                cur.execute(
-                        "EXECUTE updqstn(%(id)s, %(question)s, %(answer)s, %(created_at)s);",
-                        {
-                                "id": id_q,
-                                "question": question,
-                                "answer": answer,
-                                "created_at": created_at})
-                return False
-    except psycopg2.errors.lookup("23505"):
-        return True
+class QAWS:
+    def __init__(
+            self,
+            db_service: PgStorageService,
+            questions_service: DefaultQuestionService):
+        self.db_service: PgStorageService = db_service
+        self.questions_service: DefaultQuestionService = questions_service
 
-
-@app.teardown_request
-def close_db(_=None) -> None:  # type: ignore
-    db = g.pop("db", None)
-
-    if db is not None:
-        db.close()
-
-
-@app.route('/', methods=['POST'])
-def questions_request() -> Union[str, dict[None, None], None]:
-    if request.method == "POST":
-        questions_num: int = request.get_json().get('questions_num')  # type: ignore
+    def request_questions(self, conn: Connection) -> Union[str, dict]:
+        questions_num: int = request.get_json().get('questions_num')
         assert isinstance(questions_num, int) and questions_num >= 0, \
             f"questions_num={questions_num}"
         if questions_num == 0:
             return {}
         else:
-            questions_list = get_questions(questions_num)
-            get_db()  # type: ignore
-            prepare_updqstn()
-
-            retries: int = 1000
+            questions = self.questions_service.get_questions(questions_num)
+            retries: int = 100
             while retries > 0:
                 retries -= 1
-                fail_uniq_num: int = 0
-
-                for qstn in questions_list:
-                    fail_uniq = insert_question(
-                            qstn['id'],
-                            qstn['question'],
-                            qstn['answer'],
-                            qstn['created_at'])
-                    if fail_uniq:
-                        fail_uniq_num += 1
-
+                fail_uniq_num = (
+                        questions_num
+                        - self.db_service.insert_uniq_questions(questions, conn=conn))
                 if fail_uniq_num:
-                    questions_list = get_questions(fail_uniq_num)
+                    questions = self.questions_service.get_questions(fail_uniq_num)
+                    questions_num = fail_uniq_num
                 else:
-                    return f"{questions_list[-1]['question']}"
-
+                    return questions.question[-1]
             abort(500)
-    return None
+
+
+def init_schema(conn: Connection) -> None:
+    with conn.cursor() as cur:
+        with current_app.open_resource('schema.sql') as schema:
+            cur.execute(schema.read())
+
+
+def create_app():
+    app = Flask(__name__)
+    conn_pool = ConnectionPool(
+            conninfo="dbname='postgres'"
+                     "user='postgres'"
+                     "password='postgres'"
+                     "host='127.0.0.1'"
+                     "port='5432'",
+            open=False)
+    db_service = PgStorageService()
+    question_service = DefaultQuestionService(delegate=JSONQuestionService())
+    qaws = QAWS(
+            db_service=db_service,
+            questions_service=question_service)
+
+    def with_tx_connection(
+            pool: ConnectionPool,
+            func: Callable[[Connection], T_co]) -> T_co:
+        with pool.connection() as conn:
+            return func(conn)
+
+    def before_first_request() -> None:
+        conn_pool.open()
+        atexit.register(conn_pool.close)
+        with_tx_connection(pool=conn_pool, func=init_schema)
+    app.before_first_request(f=before_first_request)
+
+    app.add_url_rule(
+            rule="/",
+            methods=['POST'],
+            view_func=lambda: with_tx_connection(
+                    pool=conn_pool,
+                    func=qaws.request_questions))
+
+    return app
